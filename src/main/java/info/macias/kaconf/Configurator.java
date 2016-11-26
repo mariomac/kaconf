4703c11b6921c86954bacf62cefd4ad1293ea704
Mario Macias <mariomac@gmail.com>
2016-11-27 00:30:34 +0100

    added licensing

--- src/main/java/info/macias/kaconf/Configurator.java
+++ src/main/java/info/macias/kaconf/Configurator.java
@@ -1,3 +1,18 @@
+/*
+    Copyright 2016 Mario Macías
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
 package info.macias.kaconf;
 
 import java.lang.reflect.Field;
@@ -25,7 +40,7 @@ public class Configurator {
      * <p>Configures the object passed as argument. It looks for {@link Property} annotations in the
      * object passed as parameter and assigns the annotated property name present in the
      * {@link PropertySource} with the hightes priority.</p>
-     * <p>
+     *
      * <p>The invocation of this method will also configure the static fields of the class that
      * the destination object belongs to.</p>
      *
